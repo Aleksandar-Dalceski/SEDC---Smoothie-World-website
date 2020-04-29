be0b42cf9a48feac80a5f14085a9dd494c32d7e3
Aleksandar Dalceski <alek.91@hotmail.com>
2020-04-29 22:25:09 +0200

    js promena

---
+++ js/createCardElement.js
@@ -0,0 +1,26 @@
+function createCardElement(imageSrc, imageAlt, name, description, ) {
+    var card = document.createElement("div");
+    card.className = "card";
+  
+    var imageElement = document.createElement("img");
+    imageElement.src = imageSrc;
+    imageElement.alt = imageAlt;
+    imageElement.className = "card-img-top";
+  
+    var cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+  
+    var cardTitle = document.createElement("h5");
+    cardTitle.className = "card-title";
+    cardTitle.innerText = name;
+  
+    var cardText = document.createElement("p");
+    cardText.className = "card-text";
+    cardText.innerText = description;
+
+  
+    cardBody.append(cardTitle, cardText);
+    card.append(imageElement, cardBody);
+  
+    return card;
+  }
---
+++ js/protein.js
@@ -0,0 +1,38 @@
+var AllProteins;
+getAllProteins();
+function getAllProteins() {
+    fetch("https://raw.githubusercontent.com/Aleksandar-Dalceski/SEDC---Smoothie-World-website/master/data/protein.json")
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(data) {
+        AllProteins = Object.keys(data).map(function(key) {
+          var newElement = {
+            name: data[key].name,
+            image: data[key].image,
+            recipe: data[key].recipe,
+            description: data[key].description
+          };
+  
+          return newElement;
+        });
+  
+        drawAllProteinRecipes(AllProteins);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  function drawAllProteinRecipes(arrayOfProteins) {
+    arrayOfProteins.forEach(function(protein) {
+      var card = createCardElement(
+        protein.image,
+        protein.name,
+        protein.recipe,
+        protein.description,
+      );
+  
+      document.getElementById("protein").append(card);
+    });
+  }
