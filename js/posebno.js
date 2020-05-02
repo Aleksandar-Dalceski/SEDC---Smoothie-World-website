301492d37d132b1fdb3ca23ce98d0bec8cef5eb0
Aleksandar Dalceski <alek.91@hotmail.com>
2020-05-02 20:42:45 +0200

    proba

--- js/posebno.js
+++ js/posebno.js
@@ -11,13 +11,14 @@ drawEachRecepie(parse)
 function drawEachRecepie(data) {
     console.log(data)
     var recipe = `
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 35rem;">
     <img src="${data.image}" class="card-img-top" alt="image">
     <div class="card-body">
     <h5 class="card-title">${data.name}</h5>
     <p class="card-text">${data.recipe}</p>
     <p class="card-text" style="color: orange;">${data.description}</p>
      </div>
+     <img src="${data.image2}" class="card-img-top" alt="image">
 </div>
     `
     document.querySelector(".container").innerHTML = recipe;
