b5c73d5ae6657837ece47f9e4ad8cc89cab55c88
Aleksandar Dalceski <alek.91@hotmail.com>
2020-05-04 00:00:55 +0200

    bold na receptite

--- js/recipe.js
+++ js/recipe.js
@@ -11,11 +11,11 @@ drawEachRecepie(parse)
 function drawEachRecepie(data) {
     console.log(data)
     var recipe = `
-<div class="card" style="width: 60% ">
+<div class="card" style="width: 55% ">
     <img src="${data.image}" class="card-img-top" alt="image">
     <div class="card-body">
     <h1 class="card-title">${data.name}</h1>
-    <p class="card-text">${data.recipe}</p>
+    <p class="card-text" style= "font-weight:bold;">${data.recipe}</p>
     <p class="card-text" style="color: orange;">${data.description}</p>
     <img src="${data.image2}" class="card-img-top" alt="image">
     </div>
